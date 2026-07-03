ebbc86e7f0feb22c82196090bd9e66407dc4e3b8
Jamie Harper <jhuk.contact@gmail.com>
2026-07-03 01:55:12 +0100

    Update listings.js

--- bookshelf/data/listings.js
+++ bookshelf/data/listings.js
@@ -1,3 +1,29 @@
-/*
-  Initial Comment
-*/
+const bookListings = [
+    // Live Listings
+    {
+        title: "Shadows of Ruin",
+        subtitle: "The Grimshaw Saga",
+        author: "Jamie Harper",
+        description: "When a lucrative bounty leads Ander through the forgotten ruins beneath Luskan, it appears to be no different from countless hunts before it. A fugitive is on the run, the trail is fresh, and the reward is substantial enough to make the journey worthwhile.<br /><br />But beneath the City of Sails lay a world long since buried by time, and as the hunt carries him farther beneath the surface, old certainties begin to unravel.<br /><br />Loyalties are tested, hard choices must be made, and the line between honour and survival grows increasingly difficult to see.<br /><br />Some paths lead to glory.<br /><br />Others into darkness.",
+        releaseDate: "17-06-2026",
+        cover: "data/images/Shadows_of_Ruin.png",
+        links: {
+            wattpad: "https://wattpad.com/story/412638285",
+            ao3: "https://archiveofourown.org/works/86863691",
+            heyzine: null
+        }
+    },
+    {
+        title: "Arcane Requiem",
+        subtitle: null,
+        author: "Jamie Harper",
+        description: "It was the year 1013 N.D.<br /><br />A fragile calm lingered across Talandre, but in the swamplands silence spoke louder than steel. Old wards had faded, ruins stirred with whispers and that which was long since buried grew uneasy.<br /><br />Few dared to answer the silence. Fewer still returned.<br /><br />Strap in for an original Throne and Liberty fanfiction adventure.",
+        releaseDate: "31-08-2025",
+        cover: "data/images/Arcane_Requiem.png",
+        links: {
+            wattpad: "https://wattpad.com/story/400712378",
+            ao3: "https://archiveofourown.org/works/69937841",
+            heyzine: "https://heyzine.com/flip-book/3339a63d09.html"
+        }
+    }
+]
