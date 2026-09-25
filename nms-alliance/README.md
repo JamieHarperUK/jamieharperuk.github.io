fadcb59f9b0e4d24cf9b271a1f810e10f7ce8c74
oakshiftsoftware <oakshiftsoftware@gmail.com>
2026-09-25 22:03:37 +0100

    Updated NMS Alliance Template

---
+++ nms-alliance/README.md
@@ -0,0 +1,1 @@
+# No Man's Sky Alliance Showcase Website
---
+++ nms-alliance/source/main.js
@@ -0,0 +1,6 @@
+const data_source_files = {
+    alliance: "data/alliance.json",
+    bases: "data/bases.json",
+    members: "data/members.json",
+    systems: "data/systems.json"
+};
